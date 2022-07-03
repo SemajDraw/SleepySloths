781e5c648e6abc2ad0d79021dbcc98e792549495
James Ward <james.jward@hotmail.com>
2022-07-02 22:56:27 -0300

    Finished the hamburger menu

--- apps/client/src/components/layout/HamburgerMenu.tsx
+++ apps/client/src/components/layout/HamburgerMenu.tsx
@@ -1,60 +1,94 @@
-import { useState } from 'react';
-import { Box, Flex, Slide, VStack, useBreakpointValue } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import {
+  Box,
+  Flex,
+  Slide,
+  VStack,
+  useBreakpointValue,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  Input,
+  DrawerFooter,
+  Button,
+  useDisclosure,
+  Heading,
+} from '@chakra-ui/react';
 import { Slant as Hamburger } from 'hamburger-react';
 import { SECTIONS } from '../../constants';
 import { NavbarLink } from './NavbarLink';
+import { SocialButtonStack } from '../SocialButtonsStack';
 
 interface HamburgerMenuProps {
   activeSectionId: string;
 }
 
 export const HamburgerMenu = ({ activeSectionId }: HamburgerMenuProps) => {
-  const [isOpen, setOpen] = useState(false);
-  const menuW = useBreakpointValue({ base: '100vw', sm: '400px' });
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const closeDrawer = useBreakpointValue({ base: false, sm: true });
+
+  useEffect(() => {
+    if (closeDrawer) {
+      onClose();
+    }
+  }, [closeDrawer]);
+
+  const HamburgerMenu = (
+    <Flex
+      top={isOpen ? 0 : 'inherit'}
+      right={isOpen ? 0 : 'inherit'}
+      mr={isOpen ? 12 : 'inherit'}
+      h={isOpen ? '4rem' : 'inherit'}
+      position={isOpen ? 'fixed' : 'relative'}
+      zIndex={10000}
+      alignItems={'center'}
+      color={'gray.400'}
+    >
+      <Hamburger
+        toggled={isOpen}
+        toggle={isOpen ? onClose : onOpen}
+        rounded={true}
+      />
+    </Flex>
+  );
 
   return (
     <Box>
-      <Flex
-        top={isOpen ? 0 : 'inherit'}
-        right={isOpen ? 0 : 'inherit'}
-        mr={isOpen ? 12 : 'inherit'}
-        h={isOpen ? '4rem' : 'inherit'}
-        position={isOpen ? 'fixed' : 'relative'}
-        zIndex={1000}
-        alignItems={'center'}
-        color={'gray.400'}
-      >
-        <Hamburger toggled={isOpen} toggle={setOpen} rounded={true} />
-      </Flex>
-      <Slide
-        direction={'right'}
-        in={isOpen}
-        style={{ width: menuW, height: '100vh', zIndex: 10 }}
-      >
-        <Box
-          borderLeftRadius={{ base: 0, sm: 8 }}
-          w={'100%'}
-          h={'100vh'}
-          px={4}
-          bg="gray.700"
-        >
-          <VStack
-            display={{ base: 'none', md: 'flex' }}
-            direction="row"
-            spacing={6}
-          >
-            {SECTIONS.map((section) => (
-              <NavbarLink
-                key={section.id}
-                to={section.id}
-                isActive={section.id === activeSectionId}
-              >
-                {section.title}
-              </NavbarLink>
-            ))}
-          </VStack>
-        </Box>
-      </Slide>
+      {HamburgerMenu}
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose} size={'full'}>
+        <DrawerOverlay />
+        <DrawerContent bg="gray.700">
+          {HamburgerMenu}
+          <DrawerHeader pl={10}>
+            <Heading color={'white'} fontSize={'3xl'} fontWeight={700}>
+              SleepySloths
+            </Heading>
+          </DrawerHeader>
+
+          <DrawerBody pl={10}>
+            <Flex direction={'column'}>
+              {SECTIONS.map((section) => (
+                <NavbarLink
+                  key={section.id}
+                  to={section.id}
+                  isActive={section.id === activeSectionId}
+                >
+                  <Box onClick={onClose}>{section.title}</Box>
+                </NavbarLink>
+              ))}
+            </Flex>
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Flex w={'100%'} justify={'center'} pb={5}>
+              <SocialButtonStack direction={'row'} spacing={10} />
+            </Flex>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </Box>
   );
 };
